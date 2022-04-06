a9d7418b90aabc1474738de3afa83afef86aa2ea
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-06 15:12:36 -0400

    Define App component structure and import required components

--- my_budget/src/App.js
+++ my_budget/src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  
-}
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+
+const App = () => {
+  return (
+   <div className="container">
+     <h1 className="mt-3">My Budget Planner</h1>
+     <div className="row mt-3">
+       <div className="col-sm">
+         <Budget />
+       </div>
+       <div className='col-sm'>
+         <Remaining />
+       </div>
+       <div className='col-sm'>
+         <ExpenseTotal />
+       </div>
+     </div>
+   </div>
+  );
+};
 
 export default App;
