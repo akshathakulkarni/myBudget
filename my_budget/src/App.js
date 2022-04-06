2c89f9ca2e7780b933edc9612109951f81c5d068
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-06 16:54:42 -0400

    Import and render whole ExpenseList

--- my_budget/src/App.js
+++ my_budget/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
   return (
@@ -17,6 +18,12 @@ const App = () => {
        </div>
        <div className='col-sm'>
          <ExpenseTotal />
+       </div>
+       <h3 className='mt-3'>Expenses</h3>
+       <div className='row mt-3'>
+         <div className='col-sm'>
+            <ExpenseList />
+         </div>
        </div>
      </div>
    </div>
