9294d86ff9404ac4c0d9ee01ae4106544158fe1d
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-06 15:14:20 -0400

    Define a component to handle the remaining budget

---
+++ my_budget/src/components/Remaining.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Remaining = () => {
+  return (
+    <div className='alert alert-success'>
+      <span>Remaining: $1200</span>
+    </div>
+  );
+};
+
+export default Remaining;
