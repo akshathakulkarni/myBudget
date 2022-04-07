5737390945ec56432ad59ce49ba317c77fb71505
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-07 15:46:49 -0400

    Import AppContext and update the budget value

--- my_budget/src/components/Budget.js
+++ my_budget/src/components/Budget.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
+  const { budget } = useContext(AppContext);
   return (
     <div className="alert alert-secondary">
-      <span>Budget: $2000</span>
+      <span>Budget: ${budget}</span>
     </div>
   )
 }
