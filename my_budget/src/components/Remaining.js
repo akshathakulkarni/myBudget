34110e7a266a0f3ca1cf0e17ea2c6b859cb37a8a
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-08 16:01:01 -0400

    Update the function to make use of values obtained from the Context and render the alert conditionally

--- my_budget/src/components/Remaining.js
+++ my_budget/src/components/Remaining.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Remaining = () => {
+  const {expenses, budget} = useContext(AppContext);
+
+  // use reduce function to get all costs from the expenses array and add them 
+  // const sum = expenses.map(item => item.cost).reduce((prev, curr) => prev + curr, 0);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+  
   return (
-    <div className='alert alert-success'>
-      <span>Remaining: $1200</span>
+    <div className={`alert ${alertType}`}>
+      <span>Remaining: ${budget - totalExpenses}</span>
     </div>
   );
 };
