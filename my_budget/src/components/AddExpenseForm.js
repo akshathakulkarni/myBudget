af136da0999a86a1011cecf6c846b4d323bbdef9
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-08 15:14:13 -0400

    Update the input fields to include value and onChange handler and add a dispatch method for adding a new expense to the list

--- my_budget/src/components/AddExpenseForm.js
+++ my_budget/src/components/AddExpenseForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import { AppContext } from "../context/AppContext";
+import {v4 as uuidv4} from 'uuid';
 
 const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
+
+  const [name, setName] = useState('');
+  const [cost, setCost] = useState('');
+  
+  const onSubmit = (event) => {
+    event.preventDefault();
+   
+    const newExpense = {
+      id:uuidv4(),
+      name:name,
+      cost:parseInt(cost),
+    };
+    //pass dispatch actions(to update) down instead of callbacks
+    dispatch({
+      type: 'Add Expense',
+      payload: newExpense
+    });
+  };
   return (
-    <form >
+    <form onSubmit={onSubmit}>
       <div className="col-sm">
         <div className="alert alert-success">
           <label for="name">Name</label>
-          <input
-            required="required"
-            type="text"
-            className="form-control"
-            id="name"
+          <input 
+            required='required'
+            type='text'
+            className='form-control'
+            id='name'
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
       </div>
@@ -19,15 +42,17 @@ const AddExpenseForm = () => {
           <label for="cost">Cost</label>
           <input
             required="required"
-            type="text"
+            type="number"
             className="form-control"
             id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
           />
         </div>
       </div>
       <div className="row">
         <div className="col-sm">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onSubmit={onSubmit}>
             Save
           </button>
         </div>
