47a8828c025bfbadf1c900e7d8169234cb324758
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-11 14:46:53 -0400

    Add a handleDeleteExpense function to delete an expense from the list

--- my_budget/src/components/ExpenseItem.js
+++ my_budget/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {TiDelete } from 'react-icons/ti';
-
+import { AppContext } from '../context/AppContext';
+ 
 const ExpenseItem = (props) => {
-  console.log( 'INnnnn')
-  console.log('eachItem = ', props)
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: 'Delete Expense',
+      payload: props.id
+    });
+  };
+
   return (
     <li className='list-group-item d-flex justify-content-between align-items-center'>
       {props.name}
@@ -11,7 +19,7 @@ const ExpenseItem = (props) => {
         <span className='btn btn-sm btn-primary mr-3'>
           ${props.cost}
         </span> 
-        <TiDelete size='1.5em'></TiDelete>
+        <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
       </div>
     </li>
   );
