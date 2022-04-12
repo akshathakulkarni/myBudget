fbea8f9fb12a006cd1ee7bd350ef1630309da3ee
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2022-04-12 15:34:20 -0400

    Add a new case to AppReducer to update the budget

--- my_budget/src/context/AppContext.js
+++ my_budget/src/context/AppContext.js
@@ -15,6 +15,11 @@ const AppReducer = (state, action) => {
             (expense) => expense.id !== action.payload
           )
         }
+        case 'Update Budget':
+          return {
+            ...state,
+            budget: action.payload
+          }
     default:
       return state;
   }
